ee8869e4c6cb6bc55334bf0d65a6dd977539b2be
Jon <orejajonathan@gmail.com>
2020-07-23 11:24:45 +0200

    function cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -2,9 +2,30 @@ import React , {useContext} from 'react'
 import UserCart from "./context/userCart"
 import Header from './Header'
 
+import winds from './Winds'
 
 function Cart() {
     const {currentCart} = useContext(UserCart);
+    console.log(currentCart)
+
+    function currentCartWind(cart){
+        var cartWind = []
+        cart.map(element => {
+            if (cartWind(element.name)==-1){
+                cartWind.push(element.name)
+            }
+        return cartWind
+        })
+    }
+
+
+    function checkOccurence(wind, cart){
+      return cart.filter(element => element.name==wind).length
+            }
+
+        
+
+
 
     return (
         <div>
